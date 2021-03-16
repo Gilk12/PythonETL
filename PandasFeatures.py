5f50a4a4841f1329cb3b0b7e4f85cc7514db6266
gilk <s.talkachev@gmail.com>
2021-03-16 09:32:56 +0300

    add result

--- PandasFeatures.py
+++ PandasFeatures.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 flights_data = pd.read_csv("csv/flights.csv")
 
 flights_data['date'] = pd.to_datetime(flights_data['date'])
@@ -15,14 +14,8 @@ filtered_users_data = users_data[users_mask]
 
 hotels_data = pd.read_csv("csv/hotels.csv")
 
-print(flights_data[flights_mask])
-
-print(users_data[users_mask])
-
 merge_data = pd.merge(filtered_flights_data, filtered_users_data, left_on='userCode', right_on='code', how='inner')
 
 merge_data = pd.merge(merge_data, hotels_data, on='travelCode', how='inner')
 
-merge_data.to_csv("out")
-print(merge_data)
-# print(filtered_flights_data['userCode'].merge(filtered_users_data['code'], how='cross'))
+merge_data.to_parquet("result.parquet", compression='gzip')
